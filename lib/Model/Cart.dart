f49852a04dba925dfaa733d5cd7a45f069acc703
putrafir <141028423+putrafir@users.noreply.github.com>
2024-10-14 14:36:40 +0700

    make cart model

--- lib/Model/Cart.dart
+++ lib/Model/Cart.dart
@@ -1,3 +1,12 @@
 class Cart {
-  
+  final List<Map<String, String>> items = [];
+  void addToCart(Map<String, String> product) {
+    items.add(product);
+  }
+
+  List<Map<String, String>> getItems() {
+    return items;
+  }
 }
+
+final Cart cart = Cart();
