7f84b45a70f1b3f28aebf09272277666ed6d6188
putrafir <141028423+putrafir@users.noreply.github.com>
2024-10-14 13:42:40 +0700

    menambahkan routes

---
+++ lib/pages/keranjang_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KeranjangPages extends StatelessWidget {
+  const KeranjangPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/tentang_aplikasi_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TentangAplikasiPages extends StatelessWidget {
+  const TentangAplikasiPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
