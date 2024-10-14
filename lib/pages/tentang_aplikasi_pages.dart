3ef32d61b8f7b95520da7ec65914e4954b9481e2
putrafir <141028423+putrafir@users.noreply.github.com>
2024-10-14 15:05:47 +0700

    add tentang aplikasi pages

--- lib/pages/tentang_aplikasi_pages.dart
+++ lib/pages/tentang_aplikasi_pages.dart
@@ -6,7 +6,17 @@ class TentangAplikasiPages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('tentang Aplikasi')),
+      appBar: AppBar(
+        title: Text('tentang Aplikasi'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+                'Dibuat Oleh: Ahmad Putra Firdaus \nNIM: 362358302028 \nVersi: 1.0'),
+          ],
+        ),
+      ),
     );
   }
 }
